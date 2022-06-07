b4349c6b5b1249571af257adc5480c59223eaefc
penina wanjiru <penina.wanjiru@student.moringaschool.com>
2022-06-07 18:53:42 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function saturdayFun(activity="roller-skate") {
+
+    console.log(`This Saturday, I want to ${activity}!`)
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondaywork(activity="go to the office"){
+console.log(`This Monday, I will ${doWhat}.`);
+return `This Monday, I will ${doWhat}.`
+}
+
+function wrapAdjective(par1="*") {
+    //const encouragingPromptFunction = wrapAdjective("!!!")
+    return function(emphatic="special"){ 
+        return `You are ${par1}${emphatic}${par1}!` 
+
+    }
+} 
